54e240230b32b6ca15810535c47db90052b06aab
Iz P <pehisaac@gmail.com>
2019-05-23 15:47:53 -0600

    trying new packages

--- src/components/NavBizzle/NavBizzle.js
+++ src/components/NavBizzle/NavBizzle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DraggableBox from '../DraggableBox'
+import { DragDropContainer, DropTarget } from 'react-drag-drop-container';
 import './index.css'
 
 export default function NavBizzle(props) {
@@ -8,7 +9,15 @@ export default function NavBizzle(props) {
       <p>
         Demo using React Hooks - Implementing Draggable Box
     </p>
-      <DraggableBox />
+      {/* <DraggableBox /> */}
+
+      <DragDropContainer targetKey="foo" >
+        <div>Drag Me!</div>
+      </DragDropContainer>
+
+      <DropTarget targetKey="foo" >
+        <p>I'm a valid drop target for the object above since we both have the same targetKey!</p>
+      </DropTarget>
     </>
   );
 }
