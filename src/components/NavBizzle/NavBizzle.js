3389007fac227a4bdb4844a7ae5edb367391e834
Iz P <pehisaac@gmail.com>
2019-08-12 13:24:21 -0600

    finalizing navbizzle..for now

--- src/components/NavBizzle/NavBizzle.js
+++ src/components/NavBizzle/NavBizzle.js
@@ -6,14 +6,13 @@ export default function NavBizzle(props) {
     <nav>
       <div className="nav-left-container">
 
-        <a href='#'>Testing</a>
+        <a href='#'>IzzleNizzle</a>
 
       </div>
 
       <div className="nav-right-container">
-        <a href='#'>Testing</a>
-        <a href='#'>Testing</a>
-        <a href='#'>Testing</a>
+        <a href='/reactform'>React.js Form Validation</a>
+        <a href='/progress'>Progress Bar</a>
 
       </div>
     </nav>
